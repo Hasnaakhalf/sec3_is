fe8fcc35f775ddffe2e91a81b079633e053d1a5f
User <User@172.31.42.32>
2016-03-24 14:51:26 +0200

    new method added

--- src/sec3/ex1.java
+++ src/sec3/ex1.java
@@ -3,8 +3,8 @@ package sec3;
 public class ex1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	aa();
 	}
-
+public static void aa()
+{	System.out.println("asd");}
 }
